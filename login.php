a99f2a7545e594bbcc70779e98a33aa3089819fc
snowmint <starlitsky0612@gmail.com>
2019-05-30 22:06:46 +0800

    add the login.php

--- login.php
+++ login.php
@@ -10,20 +10,54 @@
     <head>
         <style>
             body {
-                /*background-image: url(pic/sketch-1474715249780.png);*/
                 background-repeat: no-repeat;
                 background-attachment: fixed;
                 background-position: center;
                 background-size: cover;
             }
+            .bg-image {
+              /* The image used */
+              background-image: url(./homepage_pic/link_6.png);
+              -webkit-filter:grayscale(1);
+              filter: blur(8px);
+              -webkit-filter:saturate(0.5);
+              -webkit-filter: blur(4px);
+              -webkit-filter:opacity(.3);
+              -webkit-filter:brightness(.6);
+
+              /* Full height */
+              height: 100%; 
+
+              /* Center and scale the image nicely */
+              background-position: center;
+              background-repeat: no-repeat;
+              background-size: cover;
+            }
             input {
                 margin: 10px;
             }
             .mylogin {
-                width: 60%;
+                
                 text-align: center;
                 margin: 0 auto;
                 background-color: #cccccc;
+            }
+            a{
+                text-decoration: none;
+            }
+            .log_form{
+                border: 15px solid rgb(85, 85, 85);
+                border: 15px solid rgba(123, 123, 123, 0.5);
+                -webkit-background-clip: padding-box; /* for Safari */
+                background-clip: padding-box; /* for IE9+, Firefox 4+, Opera, Chrome */
+                background: #ffffff;
+                text-align: center;
+                width:450px;
+                position:absolute;
+                top:40%;
+                left:40%;
+                margin: -80px 0 0 -80px;
+                vertical-align: middle;
             }
         </style>
 
@@ -33,16 +67,14 @@
         <script src="https://apis.google.com/js/platform.js?hl=en"></script>
     </head>
     <body>
-        <div class="mylogin">
-            <form class="log_form" name="form" method="post" action="./login_finish.php">
-                Email: <input type="text" name="email" /><br>
-                Password：<input type="password" name="pwd" /><br><br>
-                <input type="submit" value="Sing in" name="button" style="width:80px;height:30px;border:2px blue none;color:#fff;background: #000;text-align: center;"  onclick="return CheckFunc();">
-                <br>
-                <a href="register.php">New to ThinkSync? Click me to create new account.</a><br><br><br>
-
-                <div class="g-signin2" style="margin: 0 auto;" data-onsuccess="googleOnSignIn"></div>
-                <input type="hidden" value="normal" name="mode">
+        <div class="bg-image"></div>
+        <div class="mylogin" style="text-align: center;margin: 0 auto;">
+            <form class="log_form" name="form" method="post" action="./login_finish.php"><br>
+                Email: &nbsp;&nbsp;&nbsp;<input type="text" name="email" /><br>
+                Password: <input type="password" name="pwd" /><br><br><br>
+                <a href="register.php">New to ThinkSync? Click me to create new account.</a><br><br>
+                <input type="submit" value="Sing in" name="button" style="width:100px;height:35px;border:2px blue none;color:#fff;background: #000; margin-left: 60%;"  onclick="return CheckFunc();"><div class="g-signin2" style="margin: 0 auto; margin-left:30%;margin-top:-45px;" data-onsuccess="googleOnSignIn"></div>
+                <input type="hidden" value="normal" name="mode"><br>
             </form>
         </div>
          <script>
@@ -55,5 +87,6 @@
                 return false;
             }
         </script>
+        
     </body>
 </html>
