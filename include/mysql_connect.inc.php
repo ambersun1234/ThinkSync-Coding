e38b0407ec4342824c6ce274ce2875e42976d9da
ambersun <ambersun1019.shawn@gmail.com>
2019-09-16 16:44:10 +0800

    Fix docker error

--- include/mysql_connect.inc.php
+++ include/mysql_connect.inc.php
@@ -1,17 +1,11 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <?php
 
-/*$db_server = "localhost";
-$db_name = "beta";
-$db_user = "root";
-$db_passwd = "";
-*/
 $DBMS = 'mysqli';
-$dbname = "I4010_9608";//"I4010_tlpao";
-$dbhost = "140.129.25.238";
-//"140.129.25.238";//"i4010.cse.ttu.edu.tw";//140.12
-$dbuser = "ui3a11";
-$dbpwd = "tdhudinm0437";
+$dbname = "tsc";//"I4010_tlpao";
+$dbhost = "tsc_db_1";
+$dbuser = "root";
+$dbpwd = "1234";
 
 
 $conn = mysqli_connect($dbhost,$dbuser,$dbpwd,$dbname);
